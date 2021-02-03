c4954314e5414e0d5c2d857c3eb392de9898a62c
vishal-star-prog <62098019+vishal-star-prog@users.noreply.github.com>
2021-02-03 22:40:20 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,6 +1,19 @@
+// program to check whether a linked list is palindrome or not
+
 #include<iostream>
 #include<bits/stdc++.h>
+#include<stack>
 using namespace std;
+struct Node{
+    Node* next;
+    int data;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+}
+bool palindrome(){
+}
 int main()
 {
     
